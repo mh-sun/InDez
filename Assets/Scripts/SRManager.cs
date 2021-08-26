3c33c95846ca3f4436d51b637466895c8138c164
Mehedi Hasan Sun <bsse1025@iit.du.ac.bd>
2021-08-26 20:40:44 +0600

    bug fix : Scale rotation

--- Assets/Scripts/SRManager.cs
+++ Assets/Scripts/SRManager.cs
@@ -22,7 +22,7 @@ public class SRManager : MonoBehaviour
     {
         if (SessionData.CurrentIndex == -1)
             return;
-        if(SessionData.SpawnObject.Count <= 0)
+        if(SessionData.SpawnObject.Count > 0)
         {
             SessionData.SpawnObject[SessionData.CurrentIndex].transform.eulerAngles = new Vector3(0, RotateValue, 0);
             SessionData.SpawnObject[SessionData.CurrentIndex].transform.localScale = new Vector3(ScaleValue, ScaleValue, ScaleValue);
